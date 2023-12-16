096fb7a4c0632b7ea2a05ad939042dac20e2d632
Flash-Kaa <egor3352@yandex.ru>
2023-12-17 02:03:45 +0500

    fix checkstyle

--- src/main/java/edu/hw11/task3/Task3.java
+++ src/main/java/edu/hw11/task3/Task3.java
@@ -6,13 +6,17 @@ import net.bytebuddy.jar.asm.MethodVisitor;
 import net.bytebuddy.jar.asm.Opcodes;
 
 public class Task3 extends ClassLoader {
+    private final static String CLASS_NAME = "GeneratedClass";
+    private final static String METHOD_NAME = "fib";
+    private final static String METHOD_DESCRIPTOR = "(I)J";
+
     public static long generateFibAndGet(int n) {
         byte[] classBytes = generateClass();
         Class<?> clazz = new Task3()
-            .defineClass("GeneratedClass", classBytes, 0, classBytes.length);
+            .defineClass(CLASS_NAME, classBytes, 0, classBytes.length);
 
         try {
-            return (long) clazz.getMethod("fib", int.class).invoke(null, n);
+            return (long) clazz.getMethod(METHOD_NAME, int.class).invoke(null, n);
         } catch (Exception e) {
             throw new RuntimeException(e.getMessage());
         }
@@ -20,11 +24,11 @@ public class Task3 extends ClassLoader {
 
     private static byte[] generateClass() {
         ClassWriter cw = new ClassWriter(ClassWriter.COMPUTE_FRAMES);
-        cw.visit(Opcodes.V1_8, Opcodes.ACC_PUBLIC, "GeneratedClass",
+        cw.visit(Opcodes.V1_8, Opcodes.ACC_PUBLIC, CLASS_NAME,
             null, "java/lang/Object", null);
 
         MethodVisitor mv = cw.visitMethod(Opcodes.ACC_PUBLIC + Opcodes.ACC_STATIC,
-            "fib", "(I)J", null, null);
+            METHOD_NAME, METHOD_DESCRIPTOR, null, null);
 
         changeMV(mv);
         cw.visitEnd();
@@ -54,19 +58,19 @@ public class Task3 extends ClassLoader {
         mv.visitVarInsn(Opcodes.ILOAD, 0);
         mv.visitInsn(Opcodes.ICONST_1);
         mv.visitInsn(Opcodes.ISUB);
-        mv.visitMethodInsn(Opcodes.INVOKESTATIC, "GeneratedClass", "fib", "(I)J", false);
+        mv.visitMethodInsn(Opcodes.INVOKESTATIC, CLASS_NAME, METHOD_NAME, METHOD_DESCRIPTOR, false);
 
         // fib (n - 2)
         mv.visitVarInsn(Opcodes.ILOAD, 0);
         mv.visitInsn(Opcodes.ICONST_2);
         mv.visitInsn(Opcodes.ISUB);
-        mv.visitMethodInsn(Opcodes.INVOKESTATIC, "GeneratedClass", "fib", "(I)J", false);
+        mv.visitMethodInsn(Opcodes.INVOKESTATIC, CLASS_NAME, METHOD_NAME, METHOD_DESCRIPTOR, false);
 
         // return fib + fib
         mv.visitInsn(Opcodes.LADD);
         mv.visitInsn(Opcodes.LRETURN);
 
-        mv.visitMaxs(5, 2);
+        mv.visitMaxs(2, 2);
         mv.visitEnd();
     }
 }
